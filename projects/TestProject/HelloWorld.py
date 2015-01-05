4773d91cec4cf083e74eb273146e5e6dfdc05cf1
Derek Reiger <dreiger@hotmail.com>
2015-01-04 21:09:21 -0700

    Update HelloWorld
    Updated HelloWorld

--- projects/TestProject/HelloWorld.py
+++ projects/TestProject/HelloWorld.py
@@ -3,14 +3,14 @@ __author__ = 'dreiger'
 print ('HelloWorld!')
 
 # Sample list for loop:
-names = ["Adam","Alex","Mariah","Martine","Columbus"]
+names = ["Derek","Erin","Micah","Madeline","Leah"]
 
 for item in names:
     print item
 
 # Sample dictionary for loop:
 webster = {
-	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Aardvark" : "A star of a popular children's cartoon show.",
     "Baa" : "The sound a goat makes.",
     "Carpet": "Goes on the floor.",
     "Dab": "A small amount."
@@ -31,10 +31,10 @@ stock = {
 }
 
 prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
+    "banana": .18,
+    "apple": 1.24,
+    "orange": 1.49,
+    "pear": 1.99
 }
 
 # Write your code below!
