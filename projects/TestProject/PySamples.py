4da19f34912b96ab1d19ffbc2e42e52e4b47ade8
Derek Reiger <dreiger@hotmail.com>
2015-01-04 22:18:22 -0700

    First commits to test-branch
    Move python samples into separate file and clean up HelloWorld.

---
+++ projects/TestProject/PySamples.py
@@ -0,0 +1,46 @@
+__author__ = 'dreiger'
+
+# Sample list for loop:
+names = ["Derek","Erin","Micah","Madeline","Leah"]
+
+for item in names:
+    print item
+
+
+# Sample dictionary for loop:
+webster = {
+    "Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+for key in webster:
+    print key + ":" + webster[key]
+
+
+# Sample code for list, list processing
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": .18,
+    "apple": 1.24,
+    "orange": 1.49,
+    "pear": 1.99
+}
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+
+    return total
